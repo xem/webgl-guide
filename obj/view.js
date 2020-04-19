a4748794fecbde9b83cfce39cfdbd176d6e3509d
xem <maxime.euziere@gmail.com>
2020-04-19 11:56:18 +0200

    view

--- obj/view.js
+++ obj/view.js
@@ -3,109 +3,6 @@ deg2rad = angle => Math.PI * angle / 180;
 
 // Vertex shader
 var vshader = `
-precision mediump float;
-attribute vec4 pos;
-attribute vec4 col;
-attribute vec3 normal;
-uniform mat4 camera;
-uniform vec3 lightColor;
-uniform vec3 lightDirection;
-varying vec4 v_col;
-varying vec3 v_normal;
-varying vec3 v_position;
-void main() {
-  
-  vec4 col = vec4(1.0, 1.0, 1.0, 1.0);
-
-  // Apply the camera matrix to the vertex position
-  gl_Position = camera * pos;
-  
-  // Set varying position for the fragment shader 
-  v_position = vec3(pos.xyz);
-  
-  // Set the face normal
-  v_normal = normalize(normal);
-  
-  // Set the color
-  v_col = col;
-}`;
-
-// Fragment shader program
-var fshader = `
-precision mediump float;
-uniform vec3 lightColor;
-uniform vec3 lightPosition;
-uniform vec3 ambientLight;
-varying vec3 v_normal;
-varying vec3 v_position;
-varying vec4 v_col;
-void main() {
-  
-  // Compute direction between the light and the current point
-  vec3 lightDirection = normalize(lightPosition - v_position);
-  
-  // Compute angle between the normal and that direction
-  float nDotL = max(dot(lightDirection, v_normal), 0.0);
-  
-  // Compute diffuse light proportional to this angle
-  vec3 diffuse = lightColor * v_col.rgb * nDotL;
-  
-  // Compute ambient light
-  vec3 ambient = ambientLight * v_col.rgb;
-  
-  // Compute total light (diffuse + ambient)
-  gl_FragColor = vec4(diffuse + ambient, 1.0);
-}`;
-
-// Compile program
-var program = compile(gl, vshader, fshader);
-
-// Create a cube
-var vertices = new Float32Array(z[0].groups[0].v);
-
-var normals = new Float32Array(z[0].groups[0].vn);
-
-var n = 36;
-
-// Set position and color
-arraybuffer(gl, vertices, program, 'pos', 3, gl.FLOAT);
-arraybuffer(gl, normals, program, 'normal', 3, gl.FLOAT);
-
-// Set the clear color and enable the depth test
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.enable(gl.DEPTH_TEST);
-
-// Set the camera
-var camera = gl.getUniformLocation(program, 'camera');
-var cameraMatrix = perspective({fovy: deg2rad(30), aspect: 1, near: 1, far: 100});
-cameraMatrix = transform(cameraMatrix, {z: -5});
-gl.uniformMatrix4fv(camera, false, cameraMatrix);
-
-// Set the point light color and position
-var lightColor = gl.getUniformLocation(program, 'lightColor');
-gl.uniform3f(lightColor, .6, .6, .6);
-var lightPosition = gl.getUniformLocation(program, 'lightPosition');
-gl.uniform3f(lightPosition, 2.5, 2.5, -2.5);
-
-// Set the ambient light color
-var ambientLight = gl.getUniformLocation(program, 'ambientLight');
-gl.uniform3f(ambientLight, 0.2, 0.4, 0.2);
-
-// Animate
-setInterval(() => {
-  cameraMatrix = transform(cameraMatrix, {rx: .0, ry: .015, rz: .0});
-  gl.uniformMatrix4fv(camera, false, cameraMatrix);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, z[0].groups[0].v.length);
-}, 16);
-
-
-/*
-// Convert deg in radians
-deg2rad = angle => Math.PI * angle / 180;
-
-// Vertex shader
-var vshader = `
 attribute vec4 pos; 
 attribute vec4 col;
 attribute vec4 normal;
@@ -119,7 +16,7 @@ varying vec3 v_position;
 varying vec2 v_uv;
 void main() {
 
-  vec4 col = vec4(0.9, 0.4, 0.4, 1.0);
+  vec4 col = vec4(0.5, 0.5, 0.5, 1.0);
   
   // Apply the model matrix and the camera matrix to the vertex position
   gl_Position = mvp * pos;
@@ -162,9 +59,7 @@ void main() {
   vec3 ambient = ambientLight * v_col.rgb;
   
   // Compute total light (diffuse + ambient)
-  gl_FragColor = vec4(diffuse + ambient, 1.0);// + texture2D(sampler, v_uv);
-  
-  //gl_FragColor = ;
+  gl_FragColor = vec4(diffuse + ambient, 1.0) * vec4(texture2D(sampler, v_uv));
 }`;
 
 // Compile program
@@ -200,17 +95,17 @@ document.body.style.background = 'url('+z[0].groups[0].map_Kd+')';
 // Initialize data
 var vertices = new Float32Array(z[0].groups[0].v);
 var normals = new Float32Array(z[0].groups[0].vn);
-var normals = new Float32Array(z[0].groups[0].vt);
+var uv = new Float32Array(z[0].groups[0].vt);
 
 var n = 36;
 
 // Set position, color, normal buffers
 arraybuffer(gl, vertices, program, 'pos', 3, gl.FLOAT);
 arraybuffer(gl, normals, program, 'normal', 3, gl.FLOAT);
-arraybuffer(gl, normals, program, 'uv', 2, gl.FLOAT);
+arraybuffer(gl, uv, program, 'uv', 2, gl.FLOAT);
 
 // Set the clear color and enable the depth test
-gl.clearColor(0.8, 0.9, 1, 1);
+gl.clearColor(0, 0, 0, 1);
 gl.enable(gl.DEPTH_TEST);
 
 // Set the camera
@@ -219,14 +114,14 @@ cameraMatrix = transform(cameraMatrix, {y: -.3, z: -4, rx: 0, ry: 0, rx: .1});
 
 // Set the point light color and position
 var lightColor = gl.getUniformLocation(program, 'lightColor');
-gl.uniform3f(lightColor, .5, .5, 1);
+gl.uniform3f(lightColor, 1, 1, 1);
 
 var lightPosition = gl.getUniformLocation(program, 'lightPosition');
-gl.uniform3f(lightPosition, -2, -2, -3);
+gl.uniform3f(lightPosition, 2, 2, 3);
 
 // Set the ambient light color
 var ambientLight = gl.getUniformLocation(program, 'ambientLight');
-gl.uniform3f(ambientLight, 0.5, 0.5, 0.5);
+gl.uniform3f(ambientLight, 1, 1, 1);
 
 // Get uniforms used in the loop
 var model = gl.getUniformLocation(program, 'model');
@@ -256,4 +151,4 @@ setInterval(() => {
   // Render
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLES, 0, z[0].groups[0].v.length);
-}, 16);*/
+}, 16);
